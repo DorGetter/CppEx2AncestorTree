04502ee95d1295cdb276c7e67cb6f92da5c3c0d8
Dor Getter <dorgetter123@gmail.com>
2020-04-15 22:14:39 +0300

    Part_A

---
+++ FamilyTree.cpp
@@ -0,0 +1,285 @@
+//
+// Created by dor on 12/04/2020.
+//
+
+#include "FamilyTree.h"
+#include <iostream>
+#include <string>
+
+#define COUNT 10
+
+using namespace family;
+struct Mexception : std::exception {
+    const char* what() const throw() {
+        const char* ex = "No Such Relation In Tree";
+        return ex;
+    }
+};
+//default constructor- NODE//
+Node::Node() {}
+//creating node implementation//
+Node::Node(std::string Name) {
+    name = Name;
+    Mom = NULL;
+    Dad = NULL;
+    son = NULL;
+}
+
+/*class tree constructor -
+need to have root pointer to NULL
+*/
+Tree::Tree() {
+    Node* t_root;
+    root = t_root;
+}
+/*class tree destructor -
+need to free all memory (all nodes) in the tree
+*/
+Tree::~Tree() {
+    freeALL(root);
+    delete(root);
+}
+/*tree constructor-
+ * using name for the root
+*/
+Tree::Tree(const std::string n){
+    Node *t_root;
+    t_root = new Node(n);
+    t_root->relation="Me";
+    t_root->relation_num=0;
+    t_root->Mom=NULL;    t_root->Dad=NULL;    t_root->son=NULL;
+    root = t_root;
+}
+/*
+    adding father to a person name in the tree.
+
+*/
+Tree& Tree::addFather(std::string x, std::string x_father) {
+    Node * root_clone;
+    root_clone = root;
+    Add_Father(root_clone,x,x_father);
+    Tree& tree = *this;
+
+    return tree;
+}
+
+/*
+    recursive adding father to a node in the tree.
+*/
+void Tree::Add_Father(Node * r, std::string x, std::string x_father){
+//we reached the node to add father to.
+    if(r->name==x){
+        if(r->Dad == NULL){
+
+            Node* new_dad = new Node(x_father);
+            r->Dad = new_dad;
+            new_dad->son = r;
+            new_dad->relation_num = r->relation_num +1;
+
+            if(r->relation_num==0){
+                new_dad->relation = "father";
+            }
+
+            if(r->relation_num==1){
+                new_dad->relation = "grandfather";
+            }
+
+            if(r->relation_num > 1){
+                int i;
+                int j= new_dad->relation_num -2;
+                new_dad->relation="";
+                for(i=0; i<j; i++){
+                    new_dad->relation +="great-";
+                }
+                new_dad->relation +="grandfather";
+            }
+            return;
+        }
+
+    }
+    if(r->Dad !=NULL){Add_Father(r->Dad,x,x_father);}
+    if(r->Mom !=NULL){Add_Father(r->Mom,x,x_father);}
+}
+
+
+Tree &Tree::addMother(std::string x, std::string x_mother) {
+    Node * root_clone;
+    root_clone = root;
+    Add_Mother(root_clone,x,x_mother);
+    Tree& tree = *this;
+
+    return tree;
+}
+/*
+    recursive adding father to a node in the tree.
+*/
+void Tree::Add_Mother(Node * r, std::string x,std::string x_mother){
+//we reached the node to add father to.
+    if(r->name==x){
+        if(r->Mom == NULL){
+
+            Node* new_mom = new Node(x_mother);
+            r->Mom = new_mom;
+            new_mom->son = r;
+            new_mom->relation_num = r->relation_num +1;
+
+            if(r->relation_num==0){
+                new_mom->relation = "mother";
+            }
+
+            if(r->relation_num==1){
+                new_mom->relation = "grandmother";
+            }
+
+            if(r->relation_num > 1){
+                int i;
+                int j= new_mom->relation_num -2;
+                new_mom->relation="";
+                for(i=0; i<j; i++){
+                    new_mom->relation +="great-";
+                }
+                new_mom->relation +="grandmother";
+            }
+
+
+            return;
+        }else {
+            throw Mexception();
+        }
+
+    }
+    if(r->Dad !=NULL){Add_Mother(r->Dad,x,x_mother);}
+    if(r->Mom !=NULL){Add_Mother(r->Mom,x,x_mother);}
+    return;
+}
+
+
+std::string Tree::relation(std::string x) {
+
+    Node* r = root;
+    std::string result;
+    result =  search_relation(r,x);
+    if (result == "")
+        result = "unrelated";
+
+    return result;
+}
+
+/*
+    recursivly find the name to seek relation of.
+    returns the relation string.
+*/
+std::string Tree::search_relation(Node*r ,std::string x){
+    std::string s = r->name;
+
+    if(r->name == x) return r->relation;
+
+    else if(r->Dad==NULL && r->Mom!=NULL) return search_relation(r->Mom,x);
+    else if(r->Dad!=NULL && r->Mom==NULL) return search_relation(r->Dad,x);
+    else if(r->Dad!=NULL && r->Mom!=NULL) return search_relation(r->Dad,x)+search_relation(r->Mom,x);
+    //
+//    if(r->Dad != NULL)  search_relation(r->Dad,x);
+//    if(r->Mom != NULL)  search_relation(r->Mom,x);
+    return "";
+}
+
+//input ex. great-great-grandfather//
+std::string Tree::find(std::string x){
+    Node* t_root ;
+    t_root= root;
+    std::string result;
+    result= relationStr_ToName(t_root,x);
+    if (result=="")
+        throw Mexception();
+
+    return result;
+}
+
+std::string Tree::relationStr_ToName(Node* r, std::string x){
+    if(r->relation == x){
+        return r->name;
+    }
+
+    else if(r->Dad==NULL && r->Mom!=NULL) return relationStr_ToName(r->Mom,x);
+    else if(r->Dad!=NULL && r->Mom==NULL) return relationStr_ToName(r->Dad,x);
+    else if(r->Dad!=NULL && r->Mom!=NULL) return relationStr_ToName(r->Dad,x)+relationStr_ToName(r->Mom,x);
+    return "" ;
+}
+
+
+
+void Tree::display() {
+    Node *r = root;
+    if(r == NULL){
+        std::cout << "Tree is empty" << std::endl;
+    }
+    //send to printing//
+    print_tree(r,0);
+}
+//printing the tree recursivly//
+void Tree::print_tree(Node *root, int space){
+    // Base case
+    if (root == NULL)
+        return;
+    //incharge of spacing lelveling//
+    space += COUNT;
+
+    print_tree(root->Mom, space);
+
+    // Print current node after space
+    // count
+    std::cout<<std::endl;
+    for (int i = COUNT; i < space; i++) {
+        std::cout<<" ";
+    }
+    std::cout<<root->name<<"\n";
+
+    // Process left child
+    print_tree(root->Dad, space);
+}
+
+void Tree::freeALL(Node* temp){
+    if(temp->Mom!=NULL) freeALL(temp->Mom);
+    if(temp->Dad!=NULL) freeALL(temp->Dad);
+    if(temp->Dad==NULL && temp->Mom==NULL) free(temp);
+}
+
+void Tree::remove_sub(Node* t,std::string name){
+    if(t->Dad!=NULL && t->Dad->name==name){
+        if(t->Dad->Dad!=NULL) freeALL(t->Dad);
+        if(t->Dad->Mom!=NULL) freeALL(t->Mom);
+        delete(t->Dad);
+        t->Dad=NULL;
+        return;
+    }
+    else if(t->Mom!=NULL && t->Mom->name==name){
+        if(t->Mom->Dad!=NULL) freeALL(t->Dad);
+        if(t->Mom->Mom!=NULL) freeALL(t->Mom);
+        delete(t->Mom);
+        t->Mom=NULL;
+        return;
+    }
+    else {
+        if(t->Dad!=NULL ) remove_sub(t->Dad,name);
+        if(t->Mom!=NULL ) remove_sub(t->Mom,name);
+    }
+}
+
+void Tree::remove(std::string x) {
+    if(root->Dad!=NULL && root->Dad->name==x)  remove_sub(root,x);
+    if(root->Mom!=NULL && root->Mom->name==x)  remove_sub(root,x);
+    if(root->name==x){
+        if(root->Dad!=NULL) freeALL(root->Dad);
+        if(root->Mom!=NULL) freeALL(root->Mom);
+        delete(root);
+        return;
+    }
+    else {
+        if(root->Mom!=NULL) remove_sub(root->Mom,x);
+        if(root->Dad!=NULL) remove_sub(root->Dad,x);
+    }
+}
+
+Tree & Tree::GetTree() {
+    return *this;
+}
---
+++ Test.cpp
@@ -0,0 +1,250 @@
+//
+// Created by dor on 15/04/2020.
+//
+#define CHECK DOCTEST_CHECK
+
+#include "doctest.h"
+#include "FamilyTree.h"
+#include <string>
+using namespace family;
+
+
+
+TEST_CASE("Initializing Tree Of The British Royal Family") {
+
+
+
+/////////////////////////////////////////////////////////
+/////////////Prince Louis Tree//////////////////////////
+///////////////////////////////////////////////////////
+
+    family::Tree T("Prince Louis"); // Louis is the "root" of the tree (the youngest person).
+    T.addFather("Prince Louis", "Prince Williams")   // Tells the tree that the father of Louis is Williams.
+            .addMother("Prince Louis", "Kate Middleton");  // Tells the tree that the mother of Louis is Kate.
+
+    T.addFather("Prince Williams", "Prince Charles");
+    T.addMother("Prince Williams", "Princess Diana");
+
+    T.addFather("Kate Middleton", "Micheal Middleton");
+    T.addMother("Kate Middleton", "Carole Middleton");
+
+
+    T.addFather("Prince Charles", "King philip");
+    T.addMother("Prince Charles", "Queen Elizabeth 2");
+
+    T.addFather("Princess Diana", "John Spencer");
+    T.addMother("Princess Diana", "Frances Shand Kydd");
+
+    T.addMother("Queen Elizabeth 2", "Queen Elizabeth");
+    T.addFather("Queen Elizabeth 2", "King George 6");
+    std::cout<< "\n\t\t///////////////////////////////////////\n\t\t//////   British Royal Family   //////\n\t\t/////////////////////////////////////\n" << std::endl; 
+    T.display();
+
+    /////////////////////////
+    ////////Existence///////
+    ///////////////////////
+    CHECK(T.root);
+    CHECK(T.root->Dad);
+    CHECK(T.root->Mom);
+    CHECK(T.root->Dad->Dad);
+    CHECK(T.root->Dad->Mom);
+    CHECK(T.root->Mom->Dad);
+    CHECK(T.root->Mom->Mom);
+    CHECK(T.root->Dad->Dad->Dad);
+    CHECK(T.root->Dad->Dad->Mom);
+    CHECK(T.root->Dad->Mom->Dad);
+    CHECK(T.root->Dad->Mom->Mom);
+    CHECK(T.root->Dad->Dad->Mom->Mom);
+    CHECK(T.root->Dad->Dad->Mom->Dad);
+
+    /////////////////////////
+    ////////Rightness///////
+    ///////////////////////
+    CHECK(T.root->name == "Prince Louis");
+    CHECK(T.root->Dad->name == "Prince Williams");
+    CHECK(T.root->Mom->name == "Kate Middleton");
+    CHECK(T.root->Dad->Dad->name == "Prince Charles");
+    CHECK(T.root->Dad->Mom->name == "Princess Diana");
+    CHECK(T.root->Mom->Dad->name == "Micheal Middleton");
+    CHECK(T.root->Mom->Mom->name == "Carole Middleton");
+    CHECK(T.root->Dad->Dad->Dad->name == "King philip");
+    CHECK(T.root->Dad->Dad->Mom->name == "Queen Elizabeth 2");
+    CHECK(T.root->Dad->Mom->Dad->name == "John Spencer");
+    CHECK(T.root->Dad->Mom->Mom->name == "Frances Shand Kydd");
+    CHECK(T.root->Dad->Dad->Mom->Mom->name == "Queen Elizabeth");
+    CHECK(T.root->Dad->Dad->Mom->Dad->name == "King George 6");
+
+    /////////////////////////
+    ////////relations///////
+    ///////////////////////
+    CHECK(T.relation("Prince Louis")=="Me");
+    CHECK(T.relation("Prince Williams")=="father");
+    CHECK(T.relation("Kate Middleton")=="mother");
+    CHECK(T.relation("Prince Charles")=="grandfather");
+    CHECK(T.relation("Princess Diana")=="grandmother");
+    CHECK(T.relation("Micheal Middleton")=="grandfather");
+    CHECK(T.relation("Carole Middleton")=="grandmother");
+    CHECK(T.relation("King philip")=="great-grandfather");
+    CHECK(T.relation("Queen Elizabeth 2")=="great-grandmother");
+    CHECK(T.relation("John Spencer")=="great-grandfather");
+    CHECK(T.relation("Frances Shand Kydd")=="great-grandmother");
+    CHECK(T.relation("Queen Elizabeth")=="great-great-grandmother");
+    CHECK(T.relation("King George 6")=="great-great-grandfather");
+
+    CHECK(T.relation("Squirtel")=="unrelated");
+    CHECK(T.relation("Charizard")=="unrelated");
+    CHECK(T.relation("John Smith")=="unrelated");
+    CHECK(T.relation("captain Jack Sparrow")=="unrelated");
+
+    /////////////////////////
+    /////////find///////////
+    ///////////////////////
+    CHECK(T.find("Me")=="Prince Louis");
+    CHECK(T.find("father")=="Prince Williams");
+    CHECK(T.find("mother")=="Kate Middleton");
+    CHECK(T.find("grandfather")=="Prince CharlesMicheal Middleton");
+    CHECK(T.find("grandmother")=="Princess DianaCarole Middleton");
+    CHECK(T.find("great-grandfather")=="King philipJohn Spencer");
+    CHECK(T.find("great-grandmother")=="Queen Elizabeth 2Frances Shand Kydd");
+    CHECK(T.find("great-great-grandfather")=="King George 6");
+    CHECK(T.find("great-great-grandmother")=="Queen Elizabeth");
+
+    /////////////////////////
+    ////Throw exception/////
+    ///////////////////////
+    CHECK_THROWS(T.find("daddy"));
+    CHECK_THROWS(T.find("mommy"));
+    CHECK_THROWS(T.find("uncle"));
+    CHECK_THROWS(T.find("brother"));
+    CHECK_THROWS(T.find("sister"));
+    CHECK_THROWS(T.find("nephew"));
+    CHECK_THROWS(T.find("sibling"));
+
+    ///////////////////////////
+    /////Remove From Tree/////
+    /////////////////////////
+
+    T.remove("Prince Charles");
+    CHECK(T.relation("Queen Elizabeth")=="unrelated");
+    CHECK(T.relation("King George 6")=="unrelated");
+    CHECK(T.relation("King philip")=="unrelated");
+    CHECK(T.relation("Queen Elizabet 2")=="unrelated");
+    CHECK(T.relation("Prince Charles")=="unrelated");
+    CHECK(T.relation("Prince Williams")=="father");
+
+    //////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////New Tree- LION KING//////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+  
+      std::cout<< "\n\t\t///////////////////////////////////////\n\t\t////////   Lion King Family   ////////\n\t\t/////////////////////////////////////\n" << std::endl; 
+   
+    family::Tree L("Kiara");
+     L.addFather("Kiara", "Simba")   // Tells the tree that the father of Louis is Williams.
+            .addMother("Kiara", "Nala");  // Tells the tree that the mother of Louis is Kate.
+
+    L.addFather("Simba", "Mufasa");
+    L.addMother("Simba", "Sarabi");
+
+    L.addFather("Nala", "Unknown");
+    L.addMother("Nala", "Sarafina");
+
+    L.addFather("Mufasa", "Ahadi");
+    L.addMother("Mufasa", "Ura");
+
+    L.addFather("Ahadi", "Mohatu");
+    L.addMother("Ahadi", "Unknown2");
+
+
+    L.display();
+
+    /////////////////////////
+    ////////Existence///////
+    ///////////////////////
+    CHECK(L.root);
+    CHECK(L.root->Dad);
+    CHECK(L.root->Mom);
+    CHECK(L.root->Dad->Dad);
+    CHECK(L.root->Dad->Mom);
+    CHECK(L.root->Mom->Dad);
+    CHECK(L.root->Mom->Mom);
+    CHECK(L.root->Dad->Dad->Dad);
+    CHECK(L.root->Dad->Dad->Mom);
+
+
+
+    /////////////////////////
+    ////////Rightness///////
+    ///////////////////////
+    CHECK(L.root->name == "Kiara");
+    CHECK(L.root->Dad->name == "Simba");
+    CHECK(L.root->Mom->name == "Nala");
+    CHECK(L.root->Dad->Dad->name == "Mufasa");
+    CHECK(L.root->Dad->Mom->name == "Sarabi");
+    CHECK(L.root->Mom->Dad->name == "Unknown");
+    CHECK(L.root->Mom->Mom->name == "Sarafina");
+    CHECK(L.root->Dad->Dad->Dad->name == "Ahadi");
+    CHECK(L.root->Dad->Dad->Mom->name == "Ura");
+
+    CHECK(L.root->Dad->Dad->Dad->Mom->name == "Unknown2");
+    CHECK(L.root->Dad->Dad->Dad->Dad->name == "Mohatu");
+
+    /////////////////////////
+    ////////relations///////
+    ///////////////////////
+    CHECK(L.relation("Kiara")=="Me");
+    CHECK(L.relation("Simba")=="father");
+    CHECK(L.relation("Nala")=="mother");
+    CHECK(L.relation("Mufasa")=="grandfather");
+    CHECK(L.relation("Sarabi")=="grandmother");
+    CHECK(L.relation("Sarafina")=="grandmother");
+    CHECK(L.relation("Unknown")=="grandfather");
+    CHECK(L.relation("Ahadi")=="great-grandfather");
+    CHECK(L.relation("Ura")=="great-grandmother");
+    CHECK(L.relation("Mohatu")=="great-great-grandfather");
+    CHECK(L.relation("Unknown2")=="great-great-grandmother");
+
+    CHECK(L.relation("Scar")=="unrelated");
+    CHECK(L.relation("Zira")=="unrelated");
+    CHECK(L.relation("Kova")=="unrelated");
+
+
+    /////////////////////////
+    ////Throw exception/////
+    ///////////////////////
+    CHECK_THROWS(L.find("daddy"));
+    CHECK_THROWS(L.find("mommy"));
+    CHECK_THROWS(L.find("uncle"));
+    CHECK_THROWS(L.find("brother"));
+    CHECK_THROWS(L.find("sister"));
+    CHECK_THROWS(L.find("nephew"));
+    CHECK_THROWS(L.find("KITTEN"));
+
+    /////////////////////////
+    /////////find///////////
+    ///////////////////////
+
+    CHECK(L.find("Me")=="Kiara");
+    CHECK(L.find("father")=="Simba");
+    CHECK(L.find("mother")=="Nala");
+    CHECK(L.find("grandfather")=="MufasaUnknown");
+    CHECK(L.find("grandmother")=="SarabiSarafina");
+    CHECK(L.find("great-grandfather")=="Ahadi");
+    CHECK(L.find("great-grandmother")=="Ura");
+    CHECK(L.find("great-great-grandfather")=="Mohatu");
+    CHECK(L.find("great-great-grandmother")=="Unknown2");
+
+
+    //////////////////////////
+    /////Remove From Tree/////
+    /////////////////////////
+
+    L.remove("Mufasa");
+    CHECK(T.relation("Mufasa")=="unrelated");
+    CHECK(T.relation("Ahadi")=="unrelated");
+    CHECK(T.relation("Ura")=="unrelated");
+    CHECK(T.relation("Mohatu")=="unrelated");
+    CHECK(T.relation("Unknown2")=="unrelated");
+}
+
+
+
