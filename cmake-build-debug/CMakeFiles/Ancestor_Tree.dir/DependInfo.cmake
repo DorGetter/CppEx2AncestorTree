7b8a9751625031122594f40e662eb8c7e5eafc04
Dor Getter <dorgetter123@gmail.com>
2020-07-24 16:36:56 +0300

    Upload

---
+++ cmake-build-debug/CMakeFiles/Ancestor_Tree.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dor/Desktop/task2_AncestorTree/Ancestor-Tree/Demo.cpp" "/home/dor/Desktop/task2_AncestorTree/Ancestor-Tree/cmake-build-debug/CMakeFiles/Ancestor_Tree.dir/Demo.cpp.o"
+  "/home/dor/Desktop/task2_AncestorTree/Ancestor-Tree/FamilyTree.cpp" "/home/dor/Desktop/task2_AncestorTree/Ancestor-Tree/cmake-build-debug/CMakeFiles/Ancestor_Tree.dir/FamilyTree.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Ancestor_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Ancestor_Tree"
+  "Ancestor_Tree.pdb"
+  "CMakeFiles/Ancestor_Tree.dir/Demo.cpp.o"
+  "CMakeFiles/Ancestor_Tree.dir/FamilyTree.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ancestor_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
